f207dcc722cae1cbb4b99cb9810011841f547a3a
Marek Kejda <34581569+mkeida@users.noreply.github.com>
2020-01-22 21:24:56 +0100

    Update NLink.vue

--- boilerplate/www/client/components/NLink.vue
+++ boilerplate/www/client/components/NLink.vue
@@ -16,20 +16,20 @@
         props: ['to', 'params'],
         // Když je componenta vytvořena
         created() {
-            // Odkaz na momentální scope
-            let _this = this;
-
-            // Zažádá server o adresu
-            axios.get('/api/get-link', {
-                params: {
-                    // Cílová destinace
-                    dest: _this.to,
-                    // Query parametry cílové URL
-                    params: JSON.stringify(_this.params)
-                }
-            }).then((res) => {
-                // Aktualizace URL proměnné
-                this.url = res.data;
+            // Když je DOM načten, požádáme o link
+            Vue.nextTick(() => {
+                // Zažádá server o adresu
+                axios.get('/api/get-link', {
+                    params: {
+                        // Cílová destinace
+                        dest: this.to,
+                        // Query parametry cílové URL
+                        params: JSON.stringify(_this.params)
+                    }
+                }).then((res) => {
+                    // Aktualizace URL proměnné
+                    this.url = res.data;
+                });
             });
         }
     }
